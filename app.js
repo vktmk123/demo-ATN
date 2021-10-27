25605deee404b160da882760345cd2978f6bcd8e
Vuong Khanh Thanh <90686594+vktmk123@users.noreply.github.com>
2021-10-28 00:42:50 +0700

    thay doi

--- app.js
+++ app.js
@@ -24,13 +24,18 @@ app.get('/views',async(req,res)=>{
 app.post('/insert',async(req,res)=>{//req chua toan bo thong tin nguoi dung ;res thong tin gui ve cho nguoi dung
     const name = req.body.txtName
     const price = req.body.txtPrice
-    const url = req.body.txtURL;
+    const url = req.body.txtURL
     if (url.length == 0) {
         var result = await getAll("Products")
-        res.render('home', { products: result, picError: 'have to insert Picture!' })
-    } else {
+        res.render('home', { products: result, picError: 'have to insert Picture!'})
+    } 
+    else if (isNaN(price) == true) {
+        var result = await getAll("Products")
+        res.render('home', { products: result, pricError: 'have to insert price!'})
+    } 
+    else {
         //xay dung doi tuong insert
-        const obj = { name: name, price: price, picURL: url }//nhung thong tin can insert
+        const obj = { name: name, price: price, picURL: url}//nhung thong tin can insert
     //xay dung doi tuong insert
     await insertToDB(obj,"Products")//goi ham de insert vao db , Products from NoSQL
     res.redirect('/',)// return home page
@@ -61,6 +66,14 @@ app.get('/edit/:id',async(req,res)=>{
     const productoEdit = await getDocumentById(idproduct,"Products")
     //hien thi ra de sua
     res.render("edit",({product:productoEdit}))
+})
+
+function formatdate(date){
+    return new Date(date).toLocaleString("vi-VN")
+}
+app.get('/',(req,res)=>{
+    var td = new Date().toISOString()
+    res.render('home',{now:formatdate(td)})
 })
 
 const PORT = process.env.PORT || 5000
